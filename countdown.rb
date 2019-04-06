4fae1df9721fe77f1ad2a70825449a04148819e1
dependable-inventor-4583 <rodes88@msn.com>
2019-04-06 23:16:14 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
-#write your code here
 
-def countdown
+def countdown(number)
+  
+  while n > 0   
+  
+    puts "#{number} SECOND(S)!"
+    sleep 0.5
+    n-=1 
+  
+  end
+  
+  puts "HAPPY NEW YEAR!"
+
 end
+
